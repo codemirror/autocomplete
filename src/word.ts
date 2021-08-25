3f9002f28e4271c7c13fe97c289ba5f8ebea48e3
Marijn Haverbeke <marijn@haverbeke.nl>
2021-08-25 13:06:54 +0200

    Optimize completeAnyWord

--- src/word.ts
+++ src/word.ts
@@ -1,37 +1,79 @@
-import {CharCategory} from "@codemirror/state"
+import {Text} from "@codemirror/text"
 import {Completion, CompletionSource} from "./completion"
 
-const enum C { Range = 50000 }
+const enum C { Range = 50000, MinCacheLen = 1000, MaxList = 2000 }
+
+function wordRE(wordChars: string) {
+  let escaped = wordChars.replace(/[\\[.+*?(){|^$]/g, "\\$&")
+  try {
+    return new RegExp(`[\\p{Alphabetic}\\p{Number}_${escaped}]+`, "ug")
+  } catch {
+    return new RegExp(`[\w${escaped}]`, "g")
+  }
+}
+
+function mapRE(re: RegExp, f: (source: string) => string) {
+  return new RegExp(f(re.source), re.unicode ? "u" : "")
+}
+
+const wordCaches: {[wordChars: string]: WeakMap<Text, readonly Completion[]>} = Object.create(null)
+
+function wordCache(wordChars: string) {
+  return wordCaches[wordChars] || (wordCaches[wordChars] = new WeakMap)
+}
+
+function storeWords(doc: Text, wordRE: RegExp, result: Completion[], seen: {[word: string]: boolean}, ignoreAt: number) {
+  for (let lines = doc.iterLines(), pos = 0; !lines.next().done;) {
+    let {value} = lines, m
+    wordRE.lastIndex = 0
+    while (m = wordRE.exec(value)) {
+      if (!seen[m[0]] && pos + m.index != ignoreAt) {
+        result.push({type: "text", label: m[0]})
+        seen[m[0]] = true
+        if (result.length >= C.MaxList) return
+      }
+    }
+    pos += value.length + 1
+  }
+}
+
+function collectWords(doc: Text, cache: WeakMap<Text, readonly Completion[]>, wordRE: RegExp,
+                      to: number, ignoreAt: number) {
+  let big = doc.length >= C.MinCacheLen
+  let cached = big && cache.get(doc)
+  if (cached) return cached
+  let result: Completion[] = [], seen: {[word: string]: boolean} = Object.create(null)
+  if (doc.children) {
+    let pos = 0
+    for (let ch of doc.children) {
+      if (ch.length >= C.MinCacheLen) {
+        for (let c of collectWords(ch, cache, wordRE, to - pos, ignoreAt - pos)) {
+          if (!seen[c.label]) {
+            seen[c.label] = true
+            result.push(c)
+          }
+        }
+      } else {
+        storeWords(ch, wordRE, result, seen, ignoreAt - pos)
+      }
+      pos += ch.length + 1
+    }
+  } else {
+    storeWords(doc, wordRE, result, seen, ignoreAt)
+  }
+  if (big && result.length < C.MaxList) cache.set(doc, result)
+  return result
+}
 
 /// A completion source that will scan the document for words (using a
 /// [character categorizer](#state.EditorState.charCategorizer)), and
 /// return those as completions.
 export const completeAnyWord: CompletionSource = context => {
-  if (!context.explicit && !context.matchBefore(/\w+$/)) return null
-  let options: Completion[] = [], seen: {[word: string]: boolean} = Object.create(null)
-  let cat = context.state.charCategorizer(context.pos)
-  let start = Math.max(0, context.pos - C.Range), end = Math.min(context.state.doc.length, start + C.Range * 2)
-  let from = context.pos
-  for (let cur = context.state.doc.iterRange(start, end), pos = start; !(cur.next()).done;) {
-    let {value} = cur, start = -1
-    for (let i = 0;; i++) {
-      if (i < value.length && cat(value[i]) == CharCategory.Word) {
-        if (start < 0) start = i
-      } else if (start > -1) {
-        if (pos + start <= context.pos && pos + i >= context.pos) {
-          from = pos + start
-        } else {
-          let word = value.slice(start, i)
-          if (!seen[word]) {
-            options.push({type: "text", label: word})
-            seen[word] = true
-          }
-        }
-        start = -1
-      }
-      if (i == value.length) break
-    }
-    pos += value.length
-  }
-  return {from, options, span: /^\w*/}
+  let wordChars = context.state.languageDataAt<string>("wordChars", context.pos).join("")
+  let re = wordRE(wordChars)
+  let token = context.matchBefore(mapRE(re, s => s + "$"))
+  if (!token && !context.explicit) return null
+  let from = token ? token.from : context.pos
+  let options = collectWords(context.state.doc, wordCache(wordChars), re, C.Range, from)
+  return {from, options, span: mapRE(re, s => "^" + s)}
 }
